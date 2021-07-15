7f0cd4ea10f54bbcc0c8f110135d0632dc2ceef4
davitlabadze <labadzedato18@gmail.com>
2021-07-15 18:28:57 +0400

    update

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,26 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
-    public function comments()
-    {
-        return $this->hasMany('App\Models\Comment')->orderBy('id','desc');
-
-    }
     
     public function category()
     {
         return $this->belongsTo('App\Models\Category','cat_id');
     }
 
-    public function user()
-    {
-        return $this->belongsTo('App\Models\User');
-    }
-
-    public function post()
-    {
-       return $this->belongsTo('user_id');
-    }
+  
 
     
 }
